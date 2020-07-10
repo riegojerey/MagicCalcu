29e3dd44d3ec8e0cd41cf775b1713fb9001cb976
riegojerey <riegojere@gmail.com>
2020-07-10 11:19:03 +0800

    added buttons

--- DavidBlaineCalc.py
+++ DavidBlaineCalc.py
@@ -2,17 +2,32 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
 
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.setGeometry(500, 500, 500, 500)
+        self.setWindowTitle("David Blaine Calculator")
+        self.UI()
+        
+    def UI(self):
+        ###label
+        self.label = QtWidgets.QLabel(self)
+        self.label.setText("ano na boiiii")
+        self.label.move(100,100)
+        ###button
+        self.button1 = QtWidgets.QPushButton(self)
+        self.button1.setText("1")
+        self.button1.clicked.connect(self.clicked)
+    def clicked(self):
+        self.label.setText("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+        self.updatewidth()
+        
+    def updatewidth(self):
+        self.label.adjustSize()
+
 def window():
     app = QApplication(sys.argv)
-    win = QMainWindow()
-    win.setGeometry(500, 500, 500, 500)
-    win.setWindowTitle("David Blaine Calculator")
-    
-    
-    label = QtWidgets.QLabel(win)
-    label.setText("ano na boiiii")
-    label.move(100,100)
+    win = MyWindow()
     win.show()
     sys.exit(app.exec())
-
 window()
